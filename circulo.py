44fbb9af2f8b99ead82267897c2d692eabfe69cf
pablocalo <pcl0006@alu.medac.es>
2023-04-25 15:55:25 +0200

    cambio

---
+++ circulo.py
@@ -0,0 +1,11 @@
+class Circulo:
+    _radio:float
+
+    def __init__(self,radio:float):
+        self._radio=radio
+    
+    def area(self):
+        pass
+
+    def circunferencia(self):
+        pass
